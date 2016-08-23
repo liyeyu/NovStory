650b6c1f3416b87926c3f7fb9bedcd8638101d64
liyeyu1122@126.com <iloveyou1314>
2016-08-23 21:04:01 +0800

    百度歌词添加

--- app/src/main/java/com/liyeyu/novstory/api/BaseApi.java
+++ app/src/main/java/com/liyeyu/novstory/api/BaseApi.java
@@ -1,16 +1,22 @@
 package com.liyeyu.novstory.api;
 
+import com.liyeyu.novstory.entry.LrcBDRes;
+import com.liyeyu.novstory.entry.SongBDRes;
 import com.liyeyu.novstory.entry.LrcRes;
-import com.liyeyu.rxhttp.ApiService;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 import retrofit2.http.Url;
 
 /**
  * Created by Liyeyu on 2016/8/22.
  */
-public interface BaseApi extends ApiService {
+public interface BaseApi{
     @GET
     Call<LrcRes> get(@Url String url);
+    @GET("ting")
+    Call<SongBDRes> getBDSong(@Query("method") String method, @Query("query") String query);
+    @GET("ting")
+    Call<LrcBDRes> getBDLrc(@Query("method") String method, @Query("songid") String songid);
 }
